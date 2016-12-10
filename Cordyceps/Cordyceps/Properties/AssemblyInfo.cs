dbafec38a309a7497859da39239435688c9e33f1
Zakaria Djebbara <zadj@create.aau.dk>
2016-12-10 21:36:16 +0100

    Update #5

--- Cordyceps/Cordyceps/Properties/AssemblyInfo.cs
+++ Cordyceps/Cordyceps/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using Rhino.PlugIns;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c7ee0d75-60f8-4e7b-998e-1c7692f33e92")] // This will also be the Guid of the Rhino plug-in
